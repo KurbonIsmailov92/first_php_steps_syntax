a21b4022e467cd4761bc34ce233967e6ef07c2fc
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2022-01-28 16:21:13 +0500

    123

---
+++ hello.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body class="page_bg", color="purple">
+ <?php  $name = 'Sadaf'; $someThingToSay = 'Love you';
+        echo 'I'." ". $someThingToSay.' '. $name;
+ ?>
+ <div class="password", color="white", type="password">
+     "Hello"
+
+ </div>
+</body>
+</html>
+<?php
+$string = 'String<br>';
+$int = 123;
+$float = 10.123;
+$bool = 1;
+if ($bool==1) {
+    echo $string;
+    echo $int;
+    echo $float;
+}else{
+    echo "Fuck off";
+};
